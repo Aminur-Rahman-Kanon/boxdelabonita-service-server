c6c89976e2c5e21868445033c69d036b25a311ea
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-11-11 03:09:06 +0000

    database migration 3

--- db/db_init/db_init.js
+++ db/db_init/db_init.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOSTNAME}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: isProduction ? process.env.PG_DATABASE_URL : connectionString,
     ssl: {
         rejectUnauthorized: false,
     },
--- routes/fetchproducts.js
+++ routes/fetchproducts.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const pool = require('../db/db_init/db_init');
 
 router.get('/', async (req, res) => {
-
+    console.log('foo');
     try {
-        const products = await pool.query('SELECT * FROM product', (err, result) => {
+        await pool.query('SELECT * FROM product', (err, result) => {
+            console.log(err);
             if (err) return res.status(400).json({ status: 'failed' })
             return res.status(200).json({ status: 'success', data: result.rows })
         });
     } catch (error) {
-        return res.status(400).json({ status: 'error' });
+        return res.status(500).json({ status: 'error' });
     }
 })
 
